030d9d7f80cb88022aea06223820829851b7c5c9
bon <wjqthrtk1905@naver.com>
2021-12-07 01:04:43 +0900

    수정

--- Library/login.php
+++ Library/login.php
@@ -17,16 +17,17 @@ var_dump($_POST);*/
 
 $ID = $_POST['user_ID'];
 $PW = $_POST['password'];
-// $logout = $_POST['login'];
+$logout = $_POST['login'];
+
+session_start();
 
 $sql = "SELECT * FROM User WHERE user_ID = '{$ID}' AND password = '{$PW}'";
 // $res = $dbConnect->query($sql);
     //  $row = $res->fetch_array(MYSQLI_BOTH);
 
     if ($mysqli->query($sql)!=null) {
-        // $_SESSION['ses_userid'] = $row['User_ID'];
-        // $_SESSION['ses_userdpm'] = $row['memberdpm'];
-        echo("<script>location.replace('../php/index.html');</script>"); 
+        $_SESSION['ses_userid'] = $row['User_ID'];
+        echo("<script>location.replace('index.php');</script>"); 
     // var_dump( $_SESSION );
 
     }
