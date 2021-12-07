b5fc3de9421684fb6c27d63e5b0f72e5470194b7
qor4 <qor_4@naver.com>
2021-12-08 07:03:34 +0900

    감상문 공모전

---
+++ Library/reviewContest.php
@@ -0,0 +1,28 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pw = '';
+$dbName = 'library';
+
+$mysqli = new mysqli($host, $user, $pw, $dbName);
+
+ $mysqli -> set_charset("utf8");
+
+ $book_name=$_POST["book_name"];
+ $book_author=($_POST["book_author"]);
+ $book_review=($_POST["book_review"]);
+
+ $sql = "insert into book_review_contest (book_name, book_author , book_review)";             // (입력받음)insert into 테이블명 (column-list)
+ $sql = $sql. "values('$book_name', '$book_author', '$book_review')";         // calues(column-list에 넣을 value-list)
+
+ if($mysqli->query($sql)){                                                               //만약 sql로 잘 들어갔으면
+   echo("<script>location.replace('./index.php');</script>");                                // id님 안녕하세요.
+ }else{                                                                                //아니면
+  echo 'fail to insert sql' .$mysqli->error;                                                            //fail to insert sql로 표시
+ }
+
+mysqli_close($mysqli);
+
+
+?>
