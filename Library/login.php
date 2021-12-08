c09bf76158629c99d6a9d5bb396d913b86251643
bon <wjqthrtk1905@naver.com>
2021-12-08 12:08:48 +0900

    세션 수정

--- Library/login.php
+++ Library/login.php
@@ -1,11 +1,15 @@
+
 <?php
 
 include "include/session.php";
 include "include/dbConnect.php";
 
+/*echo "<pre>";
+var_dump($_POST);*/
 
 $ID = $_POST['user_ID'];
 $PW = $_POST['password'];
+// $logout = $_POST['login'];
 
 $sql = "SELECT * FROM User WHERE user_ID = '{$ID}' AND password = '{$PW}'";
 $res = $dbConnect->query($sql);
@@ -16,8 +20,8 @@ if ($row!=null) {
     $_SESSION['ses_username'] = $row['user_name'];
     echo("<script>location.replace('mypage.php');</script>"); 
 
-}
-if($row  == null){
-    echo("<script> location.replace('sign_in.php');alert('로그인 실패! 아이디와 비밀번호가 맞는지 다시 확인해주세요');</script>"); 
-}
+    }
+    if($row  == null){
+        echo("<script> location.replace('sign_in.php');alert('로그인 실패! 아이디와 비밀번호가 맞는지 다시 확인해주세요');</script>"); 
+    }
 ?>
