21c14b5eec5ecf1cf60f161269236be570ca0e69
jihyeon-yu <fbwlgus6@naver.com>
2021-12-11 15:27:58 +0900

    book_detail, Mypage
    book_detail, Mypage

--- Library/book_detail.php
+++ Library/book_detail.php
@@ -236,16 +236,16 @@
     <!-- Hero Section End -->
 
     <!-- Breadcrumb Section Begin -->
-    <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
+    <section class="breadcrumb-section set-bg" data-setbg="img/books-g562a90b60_1920.jpg">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <div class="breadcrumb__text">
-                        <h2>Vegetable’s Package</h2>
+                        <h2>Book Detail</h2>
                         <div class="breadcrumb__option">
                             <a href="./index.php">Home</a>
-                            <a href="./index.php">Vegetables</a>
-                            <span>Vegetable’s Package</span>
+                            <a href="./index.php">Books</a>
+                            <span>Book Detail</span>
                         </div>
                     </div>
                 </div>
@@ -258,27 +258,27 @@
     <section class="product-details spad">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6 col-md-6">
+                <div class="col-lg-4 col-md-4">
                     <div class="product__details__pic">
                         <div class="product__details__pic__item">
                             <img class="product__details__pic__item--large"
-                                src="img/product/details/product-details-1.jpg" alt="">
+                                src="https://image.aladin.co.kr/product/28271/42/cover500/k742835998_1.jpg" alt="">
                         </div>
                         <div class="product__details__pic__slider owl-carousel">
                             <img data-imgbigurl="img/product/details/product-details-2.jpg"
-                                src="img/product/details/thumb-1.jpg" alt="">
+                                src="https://image.aladin.co.kr/product/28328/90/cover500/8925579170_1.jpg" alt="">
                             <img data-imgbigurl="img/product/details/product-details-3.jpg"
-                                src="img/product/details/thumb-2.jpg" alt="">
+                                src="https://image.aladin.co.kr/product/28273/37/cover150/k852835990_1.jpg" alt="">
                             <img data-imgbigurl="img/product/details/product-details-5.jpg"
-                                src="img/product/details/thumb-3.jpg" alt="">
+                                src="https://image.aladin.co.kr/product/28182/0/cover150/k502835770_1.jpg" alt="">
                             <img data-imgbigurl="img/product/details/product-details-4.jpg"
-                                src="img/product/details/thumb-4.jpg" alt="">
+                                src="https://image.aladin.co.kr/product/28328/68/cover150/8936455737_1.jpg" alt="">
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6">
+                <div class="col-lg-8 col-md-8">
                     <div class="product__details__text">
-                        <h3>Vetgetable’s Package</h3>
+                        <h3>Book Name</h3>
                         <div class="product__details__rating">
                             <i class="fa fa-star"></i>
                             <i class="fa fa-star"></i>
@@ -287,30 +287,31 @@
                             <i class="fa fa-star-half-o"></i>
                             <span>(18 reviews)</span>
                         </div>
-                        <div class="product__details__price">$50.00</div>
-                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ac diam sit amet quam
-                            vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Vestibulum ac diam sit amet
-                            quam vehicula elementum sed sit amet dui. Proin eget tortor risus.</p>
-                        <div class="product__details__quantity">
+                        <!-- <div class="product__details__price">$50.00</div> -->
+                        <p>information</p>
+                        <!-- <div class="product__details__quantity">
                             <div class="quantity">
                                 <div class="pro-qty">
                                     <input type="text" value="1">
                                 </div>
                             </div>
-                        </div>
-                        <a href="#" class="primary-btn">ADD TO CARD</a>
-                        <a href="#" class="heart-icon"><span class="icon_heart_alt"></span></a>
+                        </div> -->
+                        <a href="#" class="primary-btn" id="btn-loan">LOAN</a>
+                        <a href="#" style="display:none;background-color: #0093FF;" class="primary-btn" id="btn-view-ebook">View Ebook</a>
+                        <a href="#" class="heart-icon" id="icon-heart-on-book"><span class="icon_heart_alt"></span></a>
+                        <p style="display:none;" id="loan-result" class="text-success">대출이 완료되었습니다.</p>
                         <ul>
-                            <li><b>Availability</b> <span>In Stock</span></li>
-                            <li><b>Shipping</b> <span>01 day shipping. <samp>Free pickup today</samp></span></li>
-                            <li><b>Weight</b> <span>0.5 kg</span></li>
-                            <li><b>Share on</b>
-                                <div class="share">
+                            <li><b>Author</b> <span>조던 B. 피터슨</span></li>
+                            <li><b>Translator</b> <span>김진주 <samp></li>
+                            <li><b>Publisher</b> <span>앵글북스</span></li>
+                            <li><b>Publication Year</b> <span>2021</span></li>
+                            <li><b>Availability</b> <span>Available</span></li>
+                                <!-- <div class="share">
                                     <a href="#"><i class="fa fa-facebook"></i></a>
                                     <a href="#"><i class="fa fa-twitter"></i></a>
                                     <a href="#"><i class="fa fa-instagram"></i></a>
                                     <a href="#"><i class="fa fa-pinterest"></i></a>
-                                </div>
+                                </div> -->
                             </li>
                         </ul>
                     </div>
@@ -318,13 +319,13 @@
                 <div class="col-lg-12">
                     <div class="product__details__tab">
                         <ul class="nav nav-tabs" role="tablist">
-                            <li class="nav-item">
+                            <!-- <li class="nav-item">
                                 <a class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab"
                                     aria-selected="true">Description</a>
-                            </li>
+                            </li> -->
                             <li class="nav-item">
                                 <a class="nav-link" data-toggle="tab" href="#tabs-2" role="tab"
-                                    aria-selected="false">Information</a>
+                                    aria-selected="false">Instruction</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" data-toggle="tab" href="#tabs-3" role="tab"
@@ -332,9 +333,9 @@
                             </li>
                         </ul>
                         <div class="tab-content">
-                            <div class="tab-pane active" id="tabs-1" role="tabpanel">
+                            <!-- <div class="tab-pane active" id="tabs-1" role="tabpanel">
                                 <div class="product__details__tab__desc">
-                                    <h6>Products Infomation</h6>
+                                    <h6>Book Instruction</h6>
                                     <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
                                         Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Vivamus
                                         suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam
@@ -353,27 +354,18 @@
                                         Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed
                                         porttitor lectus nibh. Vestibulum ac diam sit amet quam vehicula elementum
                                         sed sit amet dui. Proin eget tortor risus.</p>
-                                </div>
+                                </div> -->
                             </div>
                             <div class="tab-pane" id="tabs-2" role="tabpanel">
                                 <div class="product__details__tab__desc">
-                                    <h6>Products Infomation</h6>
-                                    <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
-                                        Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus.
-                                        Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam
-                                        sit amet quam vehicula elementum sed sit amet dui. Donec rutrum congue leo
-                                        eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat.
-                                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent
-                                        sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ac
-                                        diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante
-                                        ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                                        Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.
-                                        Proin eget tortor risus.</p>
-                                    <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem
-                                        ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet
-                                        elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum
-                                        porta. Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus
-                                        nibh. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
+                                    <h6>Book Instruction</h6>
+                                    <p>문화에는 저마다 문화의 중심을 이루고, 모든 부차적 신념의 토대가 되는 핵심 전제가 있다. </p>
+                                        이 전제를 포기하면 우리가 ‘믿고 있던 모든 것’이 허물어지고, 혼돈을 불러오는 미지가 다시 세상을 지배하기에 우리는 이것을 쉽게 포기할 수 없다.<br><br></p>
+                                        
+                                        <p>이 책은 신화와 문학, 현대 신경과학 등의 연구를 토대로, 인간이 위험천만한 ‘미탐험 영토’에서 살아가는 방법을 어떻게 배우는지, 
+                                        그리고 그 미지의 위협을 어떻게 기회로 뒤바꾸는지를 보여주며 우리 삶을 지배하고 바꾸는 ‘신념의 구조와 의미’를 재발견하도록 이끈다.</p>
+                                         이와 더불어 법과 정의, 옳고 그름의 모든 기준이 흔들리고 있는 이 시대에 과연 우리가 지키며 살아가야 하는 것이 무엇인지, 
+                                         왜 그것이 중요한지를 다시금 깨닫게 만드는 계기가 되어줄 것이다.</p>
                                 </div>
                             </div>
                             <div class="tab-pane" id="tabs-3" role="tabpanel">
@@ -463,6 +455,15 @@
         </div>
     </footer>
     <!-- Footer Section End -->
+
+    <!-- 유지현 추가 시작 -->
+    <div id="view-ebook-pop">
+        <button class="close-ebook-view"><i class="fa fa-close"></i></button>
+        <h1>Book Name</h1>
+        <hr>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore voluptatem odit, vitae nam architecto corrupti sequi accusamus. Placeat adipisci consequuntur tempore reiciendis, ab debitis possimus vitae, numquam sunt quae vel?</p>
+    </div>
+    <!-- 유지현 추가 끝 -->
 
     <!-- Js Plugins -->
     <script src="js/jquery-3.3.1.min.js"></script>
