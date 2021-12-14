cf819b48dbd988d3dd8c6a4005cdce07d816b00e
bon <wjqthrtk1905@naver.com>
2021-12-14 21:47:22 +0900

    연장 수정

--- Library/extend.php
+++ Library/extend.php
@@ -4,14 +4,25 @@ include "include/session.php";
 include "include/dbConnect.php";
 
 $bookNum = $_GET['bookNum'];
+$sql="SELECT ExtendOrNot From loan WHERE loanNum = $bookNum";
 
-$sql2="UPDATE loan SET dueDate =  date_add(duedate, interval 7 day) WHERE loanNum = $bookNum";
+$result = $dbConnect->query($sql);
+$row = mysqli_fetch_assoc($result);
 
- if($dbConnect->query($sql2)){                                                               //만약 sql로 잘 들어갔으면
-  echo("<script>location.replace('mypage.php');</script>");                                // id님 안녕하세요.
- }else{                                                                                //아니면
-  echo 'fail to insert sql' .$dbConnect->error;                                                               //fail to insert sql로 표시
- }
+if( $row['ExtendOrNot'] =='Y')
+{
+    echo("<script>alert('더이상 연장이 불가능합니다.');</script>");
+    echo("<script>location.replace('mypage.php');</script>");
+}
+else{
+    $sql2="UPDATE loan SET dueDate =  date_add(duedate, interval 7 day),ExtendOrNot= 'Y'   WHERE loanNum = $bookNum";
+    if($dbConnect->query($sql2)){     
+                                                            
+        echo("<script>location.replace('mypage.php');</script>");                              
+       }else{                                                                                //아니면
+        echo 'fail to insert sql' .$dbConnect->error;                                                               //fail to insert sql로 표시
+       }
+    }
 
 mysqli_close($dbConnect);
 
